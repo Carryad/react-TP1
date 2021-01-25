8035155d6a93bb54be3a8e90d3f21f5933a98b03
Carryad <n.david2001@laposte.net>
2021-01-25 12:42:23 +0100

    second commit

--- src/views/App.js
+++ src/views/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from '../img/logo.svg';
 import './App.css';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactt
+          Learn React
         </a>
       </header>
     </div>
