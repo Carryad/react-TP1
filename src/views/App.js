148eef36ec31c876fec080bc0d38c6b6c3c1ad6a
Carryad <n.david2001@laposte.net>
2021-01-26 10:24:22 +0100

    ajout navabar et autre

--- src/views/App.js
+++ src/views/App.js
@@ -1,25 +1,39 @@
-import logo from '../img/logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import casino from '../components/Casino'
+import Navbar from '../components/navbar/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class App extends React.Component {
+constructor(props){
+  super(props)
+
+  this.state = {
+    users: ['A','B','C'],
+    name:'Flo'
+  }
+}
+
+  componentDidMount(){
+    console.log('Component did mount')
+    console.log(this.state.name)
+    this.setState({
+      name:'tki'
+    }, () => console.log('Callback : ', this.state.name))
+    console.log(this.state.name)
+  }
+
+  render () {
+    return (
+      <Navbar/>
+      /*<div className="App">
+      {
+        this.state.users.map((user,i) => <p key={i}>{user}</p>)
+      }
+      <p>{this.state.name}</p>
+      <casino casinos={[{name: 'Monsupercasino', url:'httplol'}, {name: 'Moncasino', url:'httpmdr'}]}/>
+      </div>*/
   );
+  }
 }
 
 export default App;
